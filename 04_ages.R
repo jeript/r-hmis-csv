c992ed31f44f3dbb1e6e315b8eca22d6956ac5e7
jeript <114092992+jeript@users.noreply.github.com>
2024-07-30 08:50:52 -0500

    Update 04_ages.R

--- 04_ages.R
+++ 04_ages.R
@@ -1,18 +1,20 @@
 library(lubridate)
-library(tidyverse)
+library(dplyr)
 
-###Find age based on HMIS reporting logic
 Client_Entry_Data <- Client_Entry_Data %>%
+  group_by(PersonalID) %>%
+  mutate(MostRecentEntry = max(EntryDate)) %>%
+  ungroup() %>%
   mutate(ClientAge = ifelse(is.na(DOB), 
                             NA,
-                            ifelse(EntryDate < Export_Data$ExportStartDate,
+                            ifelse(MostRecentEntry < Export_Data$ExportStartDate,
                                    as.numeric(floor(as.period(interval(DOB, Export_Data$ExportStartDate)) / years(1))),
                                    as.numeric(floor(as.period(interval(DOB, MostRecentEntry)) / years(1))))))
 
-
-
-###Find age group
+# Find AgeGroup
 Client_Entry_Data <- Client_Entry_Data %>%
+  group_by(PersonalID) %>%
+  summarise(ClientAge = first(ClientAge)) %>%
   mutate(AgeGroup = case_when(
     is.na(ClientAge) ~ NA_character_,
     ClientAge < 5 ~ "Under 5",
@@ -27,8 +29,9 @@ Client_Entry_Data <- Client_Entry_Data %>%
     TRUE ~ NA_character_
   ))
 
-###Age Group data frame
+Client_Entry_Data$AgeGroup <- factor(Client_Entry_Data$AgeGroup, levels = c("Under 5", "5 to 12", "13 to 17", "18 to 24", "25 to 34", "35 to 44", "45 to 54", "55 to 64", "65 and over"))
+
+# Age Group data frame
 age_group_counts <- Client_Entry_Data %>%
   group_by(AgeGroup) %>%
   summarise(DistinctPersonalIDCount = n_distinct(PersonalID))
-
