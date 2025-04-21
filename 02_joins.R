f170262eb013ecf007b7da65d9ed5960f5516a1c
jeript <114092992+jeript@users.noreply.github.com>
2025-04-21 08:20:58 -0500

    Update 02_joins.R

--- 02_joins.R
+++ 02_joins.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(lubridate)
 
+
 ############################################################
 ### This will join Client, Entry, Exit, and Project data ###
 ############################################################
@@ -23,43 +24,20 @@ Project_Join <- Organization_Data %>%
 Client_Entry_Data <- Client_Entry_Data %>%
   left_join(Project_Join, by = "ProjectID")
 
-#########################################################
-### This will join just Project and Organization Data ###
-#########################################################
+### Add Living Situation Descriptions
+Client_Entry_Data <- Client_Entry_Data %>%
+  mutate(LivingSituationDescription = living_situation(LivingSituation),
+         DestinationDescription = living_situation(Destination))
 
-### Join Organization and Project Data
-Project_Join <- Organization_Data %>%
-  left_join(Project_Data, by = "OrganizationID")
+### Add Extended Living Situation Descriptions
+Client_Entry_Data <- Client_Entry_Data %>%
+  mutate(ExtLivingSituationDescription = extended_living_situation(LivingSituation),
+         ExtDestinationDescription = extended_living_situation(Destination))
 
-###Inventory filter. This will show rows where the InventoryEndDate is null
-###or is before the ExportStartDate.
-Inventory_Data_filtered <- Inventory_Data %>%
-  filter(is.na(InventoryEndDate) | InventoryEndDate < Export_Data$ExportStartDate)
+### Add Project Type Descriptions
+Client_Entry_Data <- Client_Entry_Data %>%
+  mutate(ProjectTypeDescription = project_type(ProjectType))
 
-###This will join Project and Inventory data in a new data frame
-###that will differentiate from the previous Project_Join data
-ProjectOnly_Data <- Project_Join %>%
-  left_join(Inventory_Data_filtered, by = "ProjectID")
-
-###Open funding sources filter
-Funder_Data_filtered <- Funder_Data %>%
-  filter(is.na(EndDate))
-
-###This will add Funder data to ProjectOnly_Data
-ProjectOnly_Data <- Project_Join %>%
-  left_join(Funder_Data_filtered, by = "ProjectID")
-
-#########################################
-### This will Join Client and CE Data ###
-#########################################
-
-### Join Client and Assessment Data
-Client_CE_Data <- Client_Data %>%
-  left_join(Assessment_Data, by = "PersonalID")
-
-
-### Join Client, Assessment, and Event Data
-Client_CE_Data <- Client_CE_Data %>%
-  left_join(Event_Data, by = "PersonalID", relationship = "many-to-many")
-
-
+### Add simplified Project Type Descriptions
+Client_Entry_Data <- Client_Entry_Data %>%
+  mutate(ProjectTypeDescriptionExt = project_type_ext(ProjectType))
