03eaa7ab9e8dbfdd9aacc532115679ff6672285c
jeript <114092992+jeript@users.noreply.github.com>
2025-04-21 11:19:22 -0500

    Update disability.R

--- Dashboard/disability.R
+++ Dashboard/disability.R
@@ -26,4 +26,4 @@ convert_csv_to_rds <- function(data, output_path) {
   message("Saved RDS to ", output_path)
 }
 
-convert_csv_to_rds(Disabilities_Data, "Dashboard/Disabilities_Data.rds")
+convert_csv_to_rds(Disabilities_Data, "Disabilities_Data.rds")
